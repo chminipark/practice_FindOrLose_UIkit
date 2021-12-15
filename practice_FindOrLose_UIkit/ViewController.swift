901bdf60756e02823cdb9fdeb4b994fed90df7b3
minii <minii@miniis-Mac.local>
2021-12-16 08:31:34 +0900

    First Commit

---
+++ practice_FindOrLose_UIkit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_FindOrLose_UIkit
+//
+//  Created by minii on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
