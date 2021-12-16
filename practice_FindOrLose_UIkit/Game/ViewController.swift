c9b5e0388e99758a760210a00b423cbfd1964ff3
minii <minii@miniis-Mac.local>
2021-12-16 16:42:13 +0900

    set loading icon & imageButtonAction(tag)

--- practice_FindOrLose_UIkit/Game/ViewController.swift
+++ practice_FindOrLose_UIkit/Game/ViewController.swift
@@ -9,28 +9,21 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-  
-  var subscriptions = Set<AnyCancellable>()
-  
+  // MARK: - Properties
   @IBOutlet var gameImageView: [UIImageView]!
   
   var gameImage: [UIImage] = []
   
+  var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - App State
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
-  func setImage() {
-    if gameImage.count == 4 {
-      for (idx, image) in gameImage.enumerated() {
-        gameImageView[idx].image = image
-        gameImageView[idx].contentMode = .scaleToFill
-      }
-    }
-  }
   
-  @IBAction func pressStateButton(_ sender: Any) {
+  // MARK: - IBAction
+  @IBAction func stateButtonAction(_ sender: Any) {
     
     let firstImage = UnsplashAPI.randomImage()
       .flatMap { randomImageResponse in
@@ -49,7 +42,7 @@ class ViewController: UIViewController {
         case .finished: break
         case .failure(let error):
           print("Error: \(error)")
-            
+          
         }
         
       }, receiveValue: { [unowned self] first, second in
@@ -58,26 +51,46 @@ class ViewController: UIViewController {
       })
       .store(in: &subscriptions)
     
-    
-//    UnsplashAPI.randomImage { resUnsplash in
-//      guard let url = resUnsplash?.urls.regular else {
-//        print(#fileID, "||", #function, "||", #line)
-//        return
-//      }
-//      ImageDownloader.download(url: url) { [unowned self] image in
-//        guard let image = image else {
-//          print(#fileID, "||", #function, "||", #line)
-//          return
-//        }
-//        self.gameImage.append(contentsOf: [image, image, image, image])
-//        self.gameImage.shuffle()
-//
-//        DispatchQueue.main.async {
-//          setImage()
-//        }
-//      }
-//    }
+    //    UnsplashAPI.randomImage { resUnsplash in
+    //      guard let url = resUnsplash?.urls.regular else {
+    //        print(#fileID, "||", #function, "||", #line)
+    //        return
+    //      }
+    //      ImageDownloader.download(url: url) { [unowned self] image in
+    //        guard let image = image else {
+    //          print(#fileID, "||", #function, "||", #line)
+    //          return
+    //        }
+    //        self.gameImage.append(contentsOf: [image, image, image, image])
+    //        self.gameImage.shuffle()
+    //
+    //        DispatchQueue.main.async {
+    //          setImage()
+    //        }
+    //      }
+    //    }
   }
+  
+  @IBAction func imageButtonAction(_ sender: Any) {
+    print((sender as AnyObject).tag)
+  }
+  
+  
+  // MARK: - functions
+  
+  func setImage() {
+    if gameImage.count == 4 {
+      for (idx, image) in gameImage.enumerated() {
+        gameImageView[idx].image = image
+        gameImageView[idx].contentMode = .scaleToFill
+      }
+    }
+  }
+  
+  
+  
+  
+  
   
 }
 
